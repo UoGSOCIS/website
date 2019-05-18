69e2e12c6951829a25233e5246a7b236511609ad
Marshall Asch <maasch@rogers.com>
2019-05-18 13:22:05 -0700

    added an event object to the database and made a test for most of it

---
+++ models/event/event.js
@@ -0,0 +1,184 @@
+
+"use strict";
+
+const mongoose = require("mongoose");
+const source = require("rfr");
+const schema = source("models/event/schema");
+const errors = source("models/error");
+const logger = source("logger");
+
+
+const EventModel = mongoose.model("Event", schema);
+
+class Event {
+
+    constructor() {
+        this._model = new EventModel({});
+    }
+
+    get _mongoId() {
+        return this._model._id;
+    }
+
+    get id() {
+        return this._model._id.toString();
+    }
+
+    get title() {
+        return this._model.title;
+    }
+
+    get description() {
+        return this._model.description;
+    }
+
+    get location() {
+        return this._model.location;
+    }
+
+    get startTime() {
+        return this._model.start_time;
+    }
+
+    get endTime() {
+        return this._model.end_time;
+    }
+
+    get tags() {
+        return this._model.tags;
+    }
+
+    setTitle(title) {
+        this._model.title = title;
+        return this;
+    }
+
+    setDescription(description) {
+        this._model.description = description;
+        return this;
+    }
+
+    setStartTime(start) {
+        this._model.start_time = start;
+        return this;
+    }
+
+    setEndTime(end) {
+        this._model.end_time = end;
+        return this;
+    }
+
+    setLocation(location) {
+        this._model.location = location;
+        return this;
+    }
+
+    setTags(tags) {
+        this._model.tags = tags;
+        return this;
+    }
+
+    static isValid(event) {
+        let err = new errors.event.InvalidFormatError();
+
+
+        if (typeof event.title !== "string") {
+            err.message = `Event title (${event.title}) is not a String.`;
+            return Promise.reject(err);
+        }
+
+        if (typeof event.location !== "string") {
+            err.message = `Event name (${event.location}) is not a String.`;
+            return Promise.reject(err);
+        }
+
+        if (typeof event.description !== "string") {
+            err.message = `Event description (${event.description}) is not a String.`;
+            return Promise.reject(err);
+        }
+
+        if (!(event.startTime instanceof Date)) {
+            err.message = `Event start (${event.startTime}) is not a valid date.`;
+            return Promise.reject(err);
+        }
+
+        if (!(event.endTime instanceof Date)) {
+            err.message = `Event end (${event.endTime}) is not a valid date.`;
+            return Promise.reject(err);
+        }
+
+        if (event.startTime >= event.endTime) {
+            err.message = `Event must start before it begins (${event.startTime}) > (${event.endTime}).`;
+            return Promise.reject(err);
+        }
+
+        if (!Array.isArray(event.tags)) {
+            err.message = `Event tags (${event.tags}) is not an array.`;
+            return Promise.reject(err);
+        }
+
+        return Promise.resolve(event);
+    }
+
+    static getById(id) {
+
+        return EventModel.findById(id).then((found) => {
+            if (!found) {
+                const err = new errors.event.NotFoundError(`Event ${id} was not found.`);
+                return Promise.reject(err);
+            }
+
+            let event = new Event();
+            event._model = found;
+
+            return Promise.resolve(event);
+        });
+    }
+
+
+    save() {
+        return Event.isValid(this).then(() => {
+            return this._model.save();
+        }).then(() => {
+            return this;
+        });
+    }
+
+    /**
+     * delete() removes an event from the database.
+     * @return {Promise} resolves with the event if removed successfully
+     *                   rejects with error on failure
+     */
+    delete() {
+        return EventModel.findOneAndDelete({
+            _id: this.id,
+        }).then((event) => {
+            if (!event) {
+
+                logger.warn("The event could not be deleted because it doesn't exist");
+                return Promise.reject(new errors.event.NotFoundError(`Event ${this.id} was not found.`));
+            }
+
+            return Promise.resolve(event);
+        }).catch((err) => {
+            logger.error("event (" + this.id + ") could not be deleted.", err);
+
+            return Promise.reject(err);
+        });
+    }
+
+    toApiV1() {
+        return {
+            id: this.id,
+            start_time: this.startTime,
+            end_time: this.endTime,
+            location: this.location,
+            title: this.title,
+            description: this.description,
+            tags: this.tags,
+        };
+    }
+}
+
+
+module.exports = Event;
